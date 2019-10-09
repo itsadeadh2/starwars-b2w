94a531999ea2a10fe203e36731ab7791fda90416
Thiago Barbosa da silva <thiagobarbosa@csc.gruporovema.com.br>
2019-10-09 17:07:58 -0400

    fix: sobe o server utilizando o http ao inves do app.listen

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,9 @@
 const winston = require('winston');
-const express = require('express');
-const app = express();
+const http = require('http');
+const app = require('../src/app');
+require('../src/startup/logger')();
 
 const port = process.env.PORT || 3000;
-const server = app.listen((port) => winston.info(`API running on port ${port}`));
-
-module.exports = server;
+http.createServer(app).listen(port, () => {
+  winston.info(`API running on port ${port}`);
+});
