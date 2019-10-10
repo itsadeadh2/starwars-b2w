4d61227f64f05b0f4df5eaac5c7ee4610611ad37
Thiago <thiagobarbosa@csc.gruporovema.com.br>
2019-10-09 22:30:47 -0400

    fix: corrige a ordem da checagem do data.count

--- src/repository/planet.repository.js
+++ src/repository/planet.repository.js
@@ -9,10 +9,10 @@ const getNumberOfAppearances = async (url) => {
 const postPlanet = async (objPlanet) => {
   const planet = new Planet(objPlanet);
   const { data } = await axios.default.get(`https://swapi.co/api/planets/?search=${objPlanet.nome}`);
-  const apiName = data.results[0].name.toString();
-  const postName = new RegExp(planet.nome.toString(), 'i');
   if (data.count === 0) throw new Error('Nenhum planeta encontrado com esse nome :/');
   if (data.count > 1) throw new Error('Voce precisa ser mais específico em relação ao nome...');
+  const apiName = data.results[0].name.toString();
+  const postName = new RegExp(planet.nome.toString(), 'i');
   if (!apiName.match(postName)) throw new Error('Nenhum planeta encontrado com esse nome  :/');
   planet.nome = apiName;
   planet.qtdeAparicoes = await getNumberOfAppearances(data.results[0].url);
