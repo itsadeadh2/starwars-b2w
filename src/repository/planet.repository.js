e87ad7512ce7b97b30745628f0ac7681a31af5ad
Thiago <thiagobarbosa@csc.gruporovema.com.br>
2019-10-09 20:30:30 -0400

    feat: faz o tratamento do planeta trazido da api

--- src/repository/planet.repository.js
+++ src/repository/planet.repository.js
@@ -1,11 +1,21 @@
 const axios = require('axios');
 const { Planet } = require('../models/planet.model');
 
+const getNumberOfAppearances = async (url) => {
+  const { data } = await axios.default.get(url);
+  return data.films.length;
+};
+
 const postPlanet = async (objPlanet) => {
   const planet = new Planet(objPlanet);
-  const planetFromSwApi = await axios.default.get(`https://swapi.co/api/planets/?search=${objPlanet.nome}`);
-  console.log(planetFromSwApi.body);
-  if (!planetFromSwApi.body) return null;
+  const { data } = await axios.default.get(`https://swapi.co/api/planets/?search=${objPlanet.nome}`);
+  const apiName = data.results[0].name.toString();
+  const postName = new RegExp(planet.nome.toString(), 'i');
+  if (data.count === 0) throw new Error('Nenhum planeta encontrado com esse nome :/');
+  if (data.count > 1) throw new Error('Voce precisa ser mais específico em relação ao nome...');
+  if (!apiName.match(postName)) throw new Error('Nenhum planeta encontrado com esse nome  :/');
+  planet.nome = apiName;
+  planet.qtdeAparicoes = await getNumberOfAppearances(data.results[0].url);
   await planet.save();
   return planet;
 };
