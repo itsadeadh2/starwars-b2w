f9c7e32d1a541ce4b2945bc26cd6bfba9c390276
Thiago <thiagobarbosa@csc.gruporovema.com.br>
2019-10-09 20:30:52 -0400

    feat: adiciona tratamento de erros com trycatch

--- src/controllers/planet.controller.js
+++ src/controllers/planet.controller.js
@@ -5,9 +5,12 @@ const post = async (req, res, next) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send({ message: error.details[0].message });
 
-  const planet = await planetRepo.postPlanet(req.body);
-  if (!planet) return res.send('something went wrong!');
-  return res.send('ibi');
+  try {
+    const planet = await planetRepo.postPlanet(req.body);
+    return res.send(planet);
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
 };
 
 module.exports = {
