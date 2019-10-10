5979feaced772c9498a8cda0d249b0b48ed170b6
Thiago <thiagobarbosa@csc.gruporovema.com.br>
2019-10-10 13:00:00 -0400

    feat: adiciona a rota get para retornar todos os planetas

--- src/routes/planet.routes.js
+++ src/routes/planet.routes.js
@@ -4,5 +4,6 @@ const planetController = require('../controllers/planet.controller');
 const router = express.Router();
 
 router.post('/', planetController.post);
+router.get('/', planetController.getAll);
 
 module.exports = router;
