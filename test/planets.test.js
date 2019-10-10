63beabe996c787832927a92878d424109d5e7c4b
Thiago Barbosa da silva <thiagobarbosa@csc.gruporovema.com.br>
2019-10-10 16:54:40 -0400

    feat: adiciona testes de getbyid e deletebyid

--- test/planets.test.js
+++ test/planets.test.js
@@ -74,8 +74,68 @@ describe('/planets', () => {
     it('should return a list with all the planets', async () => {
       queryParam = '';
       const res = await exec();
-      expect(res.body.length).to.be.equals(3);
+      expect(res.body.length).to.be.equals(2);
       expect(res.status).to.be.equals(200);
+    });
+  });
+
+  describe('DELETE BY ID', () => {   
+    let planetId;
+    beforeEach(async () => {
+      planeta = await request(server).post('/planets').send(planeta);
+    });
+
+    const exec = async () => await request(server).delete(`/planets/${planetId}`).send();
+
+    it('should delete the planet with the given id', async () => {
+      planetId = planeta.body._id;
+      const res = await exec();
+      expect(res.status).to.be.equals(200);
+      expect(res.body).to.have.property('nome');
+    });
+
+    it('should return a 400 if the id is invalid', async () => {
+      planetId = 'itsatrap';
+      const res = await exec();
+      expect(res.status).to.be.equals(400);
+      expect(res.body.message).to.match(/inválido/);
+    });
+
+    it('should return 404 if the planet doesnt exists', async () => {
+      planetId = '5d9e651b5682de00d2f9bd50';
+      const res = await exec();
+      expect(res.status).to.be.equals(404);
+      expect(res.body.message).to.match(/encontrado/);
+    });
+  });
+
+  describe('GET BY ID', () => {
+    let planetId;
+    beforeEach(async () => {
+      planeta = await request(server).post('/planets').send(planeta);
+    });
+
+    const exec = async () => await request(server).get(`/planets/${planetId}`).send();
+
+    it('should return a planet by its id', async () => {
+      planetId = planeta.body._id;
+      const res = await exec();
+      expect(res.status).to.be.equals(200);
+      expect(res.body).to.have.property('nome');
+    });
+
+    it('should return a 400 if the id is invalid', async () => {
+      planetId = 'itsatrap';
+      const res = await exec();
+      expect(res.status).to.be.equals(400);
+      expect(res.body.message).to.match(/inválido/);
+    });
+
+    it('should return 404 if the planet doesnt exists', async () => {
+      planetId = '5d9e651b5682de00d2f9bd50';
+      const res = await exec();
+      expect(res.status).to.be.equals(404);
+      expect(res.body.message).to.match(/encontrado/);
     });
   });
 });
